cfeda5eb12c198890746aa92d7bd78f2a4b4bb66
ashwabu <abdulazizredi90@gmail.com>
2023-07-11 04:45:10 +0300

    resize image

--- script.py
+++ script.py
@@ -2,7 +2,8 @@ import cv2
 
 img = cv2.imread('galaxy.jpg',0) #read the image
 
-show_galaxy = cv2.imshow('glaxy',img) #show the image
+resize_image=cv2.resize(img,(1000,500))
+show_galaxy = cv2.imshow('glaxy',resize_image) #show the image
 
 cv2.waitKey(2000)
 cv2.destroyAllWindows()
