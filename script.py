58a88ca00ff046d8ec54f7b0b7038137440ac164
ashwabu <abdulazizredi90@gmail.com>
2023-07-11 04:41:33 +0300

    display image

--- script.py
+++ script.py
@@ -1,3 +1,8 @@
 import cv2
 
-img = cv2.imread('galaxy.jpg')
+img = cv2.imread('galaxy.jpg',0) #read the image
+
+show_galaxy = cv2.imshow('glaxy',img) #show the image
+
+cv2.waitKey(2000)
+cv2.destroyAllWindows()
