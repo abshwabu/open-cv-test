1a455f72bc85344547a4ecba5b8c50e83cfc49a4
ashwabu <abdulazizredi90@gmail.com>
2023-07-11 04:53:22 +0300

    edited

--- script.py
+++ script.py
@@ -2,8 +2,8 @@ import cv2
 
 img = cv2.imread('galaxy.jpg',0) #read the image
 
-hight = img.shape[0]
-width = img.shape[1]
+hight = img.shape[1]
+width = img.shape[0]
 
 resize_image=cv2.resize(img,(int(hight/2),int(width/2)))
 show_galaxy = cv2.imshow('glaxy',resize_image) #show the image
